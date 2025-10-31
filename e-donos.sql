8e4db03a8034182f79f3aa89496c2f3289bef477
Aleksander-Drazkowski <aleksander.drazkowski@zspswiecie.pl>
2025-10-31 12:17:02 +0100

    sql

---
+++ e-donos.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Paź 31, 2025 at 12:16 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `e-donos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zgloszenia`
+--
+-- Error reading structure for table e-donos.zgloszenia: #1932 - Table &#039;e-donos.zgloszenia&#039; doesn&#039;t exist in engine
+-- Error reading data for table e-donos.zgloszenia: #1064 - Something is wrong in your syntax obok &#039;FROM `e-donos`.`zgloszenia`&#039; w linii 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
